dab07b20d2161ebd5ba906267a6be60155d84b71
Sirben <93244935+Ben-Sicat@users.noreply.github.com>
2023-03-06 09:38:29 +0000

    here

--- calculations.py
+++ calculations.py
@@ -8,10 +8,10 @@ class calculations:
         self.relative_chopped = 0
         self.relative_rounded = 0
         
-    def euler_num(exponent):
+    def euler_num(self, exponent):
         temp = np.exp(exponent)
         return temp
-    def pi_num(exponent):
+    def pi_num(self,exponent):
         temp = np.pi(exponent)
         return temp  
     def relative_error(self, t_value, a_value):
@@ -19,11 +19,11 @@ class calculations:
         r_error *= r_error
         return r_error
 
-    def chopped(num, x):
+    def chopped(self,num, x):
         chopped_num = math.floor(num * (10**x) / (10 ** x))
         return chopped_num
     
-    def rounded(num, x):
+    def rounded(self,num, x):
         rounded_num = round(num, x)
         return rounded_num
     
